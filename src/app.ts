b6c56a42dd9a3e3c3395cad2dc92b2d07ee84f05
arye shustimov <arye_s@optimove.com>
2024-02-18 18:32:51 +0200

    add rate limit

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,23 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import router from './routes';
+import { rateLimit } from 'express-rate-limit'
 
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
 
 // Create Express server
 const app = express(); // New express instance
 const port = 3000; // Port number
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 // Express configuration
 app.use(cors()); // Enable CORS
@@ -15,8 +27,6 @@ app.use(helmet()); // Enable Helmet
 app.use(morgan('dev')); // Enable Morgan
 app.use(express.json()); // <=== Enable JSON body parser
 
-
-console.log("arye")
 // Define Express routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
